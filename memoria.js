fb776253b36bf242a22cf52765f3c2ae54b72f64
nickmit21 <131312081+nickmit21@users.noreply.github.com>
2024-11-28 17:29:55 -0300

    Add files via upload

---
+++ memoria.js
@@ -0,0 +1,27 @@
+elefante=<img src="https://conafer.org.br/wp-content/uploads/2022/10/566f045d0e2163522f00f274thinkstockphotos-479667835-1-scaled.webp"></img>
+tartaruga=<img src="https://super.abril.com.br/wp-content/uploads/2018/05/humanidade-causa-cc3a2ncer-em-animais-selvagens.png"></img>
+passaro=<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbM5TWWCp84uthGMfP4-eIAXsHMTpQGLH4Jw&s"></img>
+lobo=<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm1glF44iOsh1ZKV19m9ze0xu1xpXGiCt2-g&s"></img>
+golfinho=<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-sZ0idD1Ti6MxQILHTdPWnJeYMf-tdSskFg&s"></img>
+coala=<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIdLS92V7qyeZSmq3-2h6zcHC2RahcH6Y9iw&s"></img>
+lagarto=<img src="https://petanjo.com/blog/wp-content/uploads/2021/12/animais-de-estima%C3%A7%C3%A3o-ex%C3%B3ticos-1.jpg"></img>
+coruja=<img src="https://s2-vidadebicho.glbimg.com/mNI9ToukK5NC5d3pbwch4YRsNhM=/0x0:1079x720/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_fb623579cd474803aedbbbbae014af68/internal_photos/bs/2022/0/I/V9X24JTxAGhmgell4bTg/2022-05-13-1-morcegos-imitam-o-zumbido-de-vespas-para-assustar-predadores.jpeg"></img>
+tatu=<img src="https://mega.ibxk.com.br/2023/03/01/01000010106688.jpg"></img>
+onça=<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNwTfP0KVPfRBPIkaNWuOyGBmVN0TzlXMSA&s"></img>
+capivara=<img src="https://cdn.acritica.net/img/pc/920/600/dn_arquivo/2023/03/unnamed_2.jpg"></img>
+gorila=<img src="https://s2-umsoplaneta.glbimg.com/1037GcVAUfXt1bF4bSZCXjN5Fh4=/0x0:4459x2973/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_7d5b9b5029304d27b7ef8a7f28b4d70f/internal_photos/bs/2021/0/R/TuzrYcQfArntKSDr4h6g/gettyimages-533801804.jpg"></img>
+cobra=<img src="https://content.nationalgeographic.pt/medio/2024/04/22/shutterstock-26879694-2327358941-240415103209_0a78bed5_240422172516_1280x854.jpg"></img>
+cachorro=<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8YCbLchWBk9jQ1mkgk0An8SjFpJz88cEzNA&s"></img>
+coelho=<img src="https://www.petz.com.br/blog/wp-content/uploads/2018/10/dente-de-coelho.jpg"></img>
+peixe=<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsNlM7HyYPGATtnOC8Hg22ov6EqpLykN49qg&s"></img>
+
+animais=[elefante,tartaruga,passaro,lobo,golfinho,coala,lagarto,coruja,tatu,onça,capivara,gorila,cobra,cachorro,coelho,peixe]
+
+function ca() {
+	
+    for(i=0;i<16;i++) {
+    
+        animais.push(Math.floor((Math.random() * 16) + 1)); 	
+
+    }
+}
